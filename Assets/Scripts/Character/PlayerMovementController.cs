6dd81e92a8c013aaf83bb718dc3ffc173ed2d8c0
IncubxU <denis.a.shevtsov@gmail.com>
2022-11-04 14:47:36 +0300

    Improved rotations and isGrounded condition
    Подправил развороты ( не изменяют размеров модели)
    Проверка "Заземленности" выполняется через rayCast

--- Assets/Scripts/Character/PlayerMovementController.cs
+++ Assets/Scripts/Character/PlayerMovementController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Collections;
 
 public class PlayerMovementController : MonoBehaviour
 {
@@ -8,10 +7,14 @@ public class PlayerMovementController : MonoBehaviour
 	public float minFall = -1.5f;
 	public float gravity = -9.8f;
 	public float terminalVelocity = -10f;
-
+	public float fallAcceleration = 10f;
+	public bool hitGround = false;
 
 	private CharacterController _characterController;
 	private float vertSpeed;
+
+
+	private ControllerColliderHit contact;
 	void Start()
     {
 		vertSpeed = minFall;
@@ -21,14 +24,21 @@ public class PlayerMovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
 		Vector3 movement = Vector3.zero;
 		float deltaZ = Input.GetAxis("Horizontal") * speed;
 		movement = Vector3.forward * deltaZ;
-		
 
 
-		if (_characterController.isGrounded)
+		hitGround = false;
+		RaycastHit hit;
+		if (vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit))
+		{
+			float check = (_characterController.height + _characterController.radius) / _characterController.height/ 1.9f;
+			hitGround = hit.distance <= check;
+		}
+
+
+		if (hitGround)
 		{
 			if (Input.GetKeyDown(KeyCode.Space))
 			{
@@ -41,23 +51,42 @@ public class PlayerMovementController : MonoBehaviour
 		}
 		else
 		{
-			vertSpeed += gravity * 5 * Time.deltaTime;
+			vertSpeed += gravity * fallAcceleration * Time.deltaTime;
 			if (vertSpeed < terminalVelocity)
 			{
 				vertSpeed = terminalVelocity;
 			}
-		}
-		movement.y = vertSpeed;
+
+            if (_characterController.isGrounded)
+            {
+                if (Vector3.Dot(movement, contact.normal) < 0)
+                {
+                    movement = contact.normal * speed;
+                }
+				else
+				{
+					movement += contact.normal * speed;
+				}
+			}
+        }
+
+			movement.y = vertSpeed;
 
 
-		Vector3 pScale = Vector3.one;
+		Vector3 pScale = transform.localScale;
 		if (!Mathf.Approximately(deltaZ, 0))
 		{
-			transform.localScale = new Vector3(Mathf.Sign(deltaZ) / pScale.x, 1 / pScale.y, 1);
+			pScale.x = Mathf.Sign(deltaZ) * Mathf.Abs(pScale.x);
+			transform.localScale = pScale;
 		}
 
 
 		movement = movement *= Time.deltaTime;
 		_characterController.Move(movement);
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		contact = hit;
 	}
 }
