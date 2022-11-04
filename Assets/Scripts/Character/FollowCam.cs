59c71b4a9d2f8d0a1767b4ad5ff7a54597bf8f34
IncubxU <denis.a.shevtsov@gmail.com>
2022-11-04 16:07:30 +0300

    Refactored code. X is not horizontal direction

--- Assets/Scripts/Character/FollowCam.cs
+++ Assets/Scripts/Character/FollowCam.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class FollowCam : MonoBehaviour {
-	public Transform target;
-	public float smoothTime = 0.2f;
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.2f;
 
-	private Vector3 _velocity = Vector3.zero;
-	private Vector3 offset;
+    private Vector3 _velocity = Vector3.zero;
+    private Vector3 offset;
 
-	// Use this for initialization
-	void Start() {
-		offset = transform.position - target.position;
-	}
-	
-	// Update is called once per frame
-	void LateUpdate() {
-		Vector3 targetPosition = new Vector3(transform.position.x, target.position.y + offset.y, target.position.z + offset.z);
-		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
+    }
 }
--- Assets/Scripts/Character/PlayerMovementController.cs
+++ Assets/Scripts/Character/PlayerMovementController.cs
@@ -3,58 +3,49 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     public float speed = 4.5f;
-	public float jumpSpeed = 15.0f;
-	public float minFall = -1.5f;
-	public float gravity = -9.8f;
-	public float terminalVelocity = -10f;
-	public float fallAcceleration = 10f;
+    public float jumpSpeed = 12.0f;
+    public float minFall = -1.5f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10f;
+    public float fallAcceleration = 5f;
 
-	private CharacterController _characterController;
-	private float vertSpeed;
+    private CharacterController _characterController;
+    private float vertSpeed;
+    private ControllerColliderHit contact;
 
 
-	private ControllerColliderHit contact;
-	void Start()
+    void Start()
     {
-		vertSpeed = minFall;
-		_characterController = GetComponent<CharacterController>();
+        vertSpeed = minFall;
+        _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		Vector3 movement = Vector3.zero;
-		float deltaZ = Input.GetAxis("Horizontal") * speed;
-		movement = Vector3.forward * deltaZ;
+        Vector3 movement = Vector3.zero;
 
+        float horizontalMovement = GetHorizontalMovement();
+        movement.x = horizontalMovement;
 
-		bool hitGround = false;
-		RaycastHit hit;
-		if (vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit))
-		{
-			float check = (_characterController.height + _characterController.radius) / _characterController.height/ 1.9f;
-			hitGround = hit.distance <= check;
-		}
-
-
-		if (hitGround)
-		{
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				vertSpeed = jumpSpeed;
-			}
-			else
-			{
-				vertSpeed = minFall;
-			}
-		}
-		else
-		{
-			vertSpeed += gravity * fallAcceleration * Time.deltaTime;
-			if (vertSpeed < terminalVelocity)
-			{
-				vertSpeed = terminalVelocity;
-			}
+        if (CheckGroundHit())
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                vertSpeed = minFall;
+            }
+        }
+        else
+        {
+            vertSpeed += gravity * fallAcceleration * Time.deltaTime;
+            if (vertSpeed < terminalVelocity)
+            {
+                vertSpeed = terminalVelocity;
+            }
 
             if (_characterController.isGrounded)
             {
@@ -62,30 +53,47 @@ public class PlayerMovementController : MonoBehaviour
                 {
                     movement = contact.normal * speed;
                 }
-				else
-				{
-					movement += contact.normal * speed;
-				}
-			}
+                else
+                {
+                    movement += contact.normal * speed;
+                }
+            }
+        }
+        movement.y = vertSpeed;
+
+        //TODO correct rotation mechanism
+        Vector3 pScale = transform.localScale;
+        if (!Mathf.Approximately(horizontalMovement, 0))
+        {
+            pScale.x = Mathf.Sign(horizontalMovement) * Mathf.Abs(pScale.x);
+            transform.localScale = pScale;
         }
 
-			movement.y = vertSpeed;
 
-		//TODO correct rotation mechanism
-		Vector3 pScale = transform.localScale;
-		if (!Mathf.Approximately(deltaZ, 0))
-		{
-			pScale.x = Mathf.Sign(deltaZ) * Mathf.Abs(pScale.x);
-			transform.localScale = pScale;
-		}
+        movement *= Time.deltaTime;
+        _characterController.Move(movement);
+    }
 
+    private float GetHorizontalMovement()
+    {
+        return Input.GetAxis("Horizontal") * speed;
+    }
 
-		movement = movement *= Time.deltaTime;
-		_characterController.Move(movement);
-	}
+    private bool CheckGroundHit()
+    {
+        bool hitGround = false;
+        RaycastHit hit;
+        if (vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            float check = (_characterController.height + _characterController.radius) / _characterController.height / 1.9f;
+            hitGround = hit.distance <= check;
+        }
 
-	void OnControllerColliderHit(ControllerColliderHit hit)
-	{
-		contact = hit;
-	}
+        return hitGround;
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        contact = hit;
+    }
 }
